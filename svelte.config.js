0092759a25cfa76d5308edde172a9ecad42027b1
tom <tom@myrddin.home>
2026-09-12 18:56:22 +0200

    qdd svelte config

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from 'svelte-adapter-foo';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// adapter options go here
+		})
+	}
+};
+
+export default config;
